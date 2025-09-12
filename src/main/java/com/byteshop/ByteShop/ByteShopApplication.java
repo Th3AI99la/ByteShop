12ea99731cdaf9baaec9059d99129525929215e2
Th3AI99la <talles.th54@gmail.com>
2025-09-11 22:51:34 -0300

    first commit

---
+++ src/main/java/com/byteshop/ByteShop/ByteShopApplication.java
@@ -0,0 +1,13 @@
+package com.byteshop.ByteShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteShopApplication.class, args);
+	}
+
+}
